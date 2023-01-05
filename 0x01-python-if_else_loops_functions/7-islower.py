efd0c0fb343146634955865295f1a81ba610ecf1
PyBrown <osinaikeanuola@gmail.com>
2023-01-05 03:40:06 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) in range(65, 91):
+    if ord(c) in range(97, 123):
         return True
-    elif ord(c) in range(97, 123):
+    else:
         return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for char in str:
         if ord(char) in range(97, 123):
             char = chr(ord(char)-32)
-        print(f'{char}', end='')
+        print('{}'.format(char), end='')
     print('')
